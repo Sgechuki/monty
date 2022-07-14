efb1170a954e71a80ca3fbbeb390de0981acd928
Sgechuki <sgechuki@outlook.com>
2022-07-14 12:24:29 -0700

    Fix seg fault

--- monty.h
+++ monty.h
@@ -1,10 +1,9 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 #define OPCODES { \
 		{"push", push},\
@@ -15,9 +14,6 @@
 		{NULL, NULL} \
 	};
 
-void errargc(void);
-void erropen(char *s);
-
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -25,7 +21,7 @@ void erropen(char *s);
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
@@ -35,18 +31,19 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
+ * struct instruction_s - opcoode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
 /**
  * struct omni_p - used to pass data to "push" function
  * @value: pointer to value of new stack
@@ -58,6 +55,8 @@ typedef struct omni_p
 	char *value;
 } omni;
 omni global;
+void errargc(void);
+void erropen(char *s);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number __attribute__((unused)));
 stack_t *add_node(stack_t **stack, int i);
