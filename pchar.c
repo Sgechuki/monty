88f1a1fecb5910b7115a6c0e7fd13fac107e0c9d
Sgechuki <sgechuki@outlook.com>
2022-07-17 01:54:11 -0700

    Change ascii range

--- pchar.c
+++ pchar.c
@@ -8,19 +8,24 @@
  */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp;
-
 	if (!stack || !(*stack))
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	else if ((*stack)->n < 0 && (*stack)->n > 255)
+	else if ((*stack)->n >= 65 && (*stack)->n <= 90)
 	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
-		exit(EXIT_FAILURE);
+		putchar((*stack)->n);
+		putchar('\n');
 	}
-	temp = *stack;
-	putchar(temp->n);
-	putchar('\n');
+	else if ((*stack)->n >= 97 && (*stack)->n <= 122)
+	{
+		putchar((*stack)->n);
+		putchar('\n');
+	}
+	else
+	{
+	fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+	exit(EXIT_FAILURE);
+	}
 }
